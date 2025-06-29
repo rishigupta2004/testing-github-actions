560dbfa76fac72c62187bd56a03060a93d181339
Rishi <rishigupta.rg007@gmail.com>
2025-06-29 18:35:46 +0530

    test cases

--- src/mathOperation.py
+++ src/mathOperation.py
@@ -1,17 +1,17 @@
-def add(a,b):
-    return a+b;
+def add(a, b):
+    return a + b
 
-def sub(a,b):
-    return a-b;
+def sub(a, b):
+    return a - b
 
-def multiply(a,b):
-    return a*b;
+def multiply(a, b):
+    return a * b
 
-def divide(a,b):
-    return a/b;
+def divide(a, b):
+    return a / b
 
-def modulous(a,b):
-    return a%b;
+def modulous(a, b):
+    return a % b
 
-def pow(a,b):
-    return a^b;
+def pow(a, b):
+    return a ** b  # <-- NOT a^b (that's bitwise XOR)
